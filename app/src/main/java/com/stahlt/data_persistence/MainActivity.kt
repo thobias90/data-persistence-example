51908d687da42f2ccdd761ffe44328ade40b598b
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2023-08-26 14:34:01 -0300

    feat: add variables and find it into view

--- app/src/main/java/com/stahlt/data_persistence/MainActivity.kt
+++ app/src/main/java/com/stahlt/data_persistence/MainActivity.kt
@@ -3,11 +3,19 @@ package com.stahlt.data_persistence
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var etCod: EditText
+    private lateinit var etName: EditText
+    private lateinit var etPhone: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etCod = findViewById(R.id.etCod)
+        etName = findViewById(R.id.etName)
+        etPhone = findViewById(R.id.etPhone)
     }
 
     fun btAddOnClick(view: View) {}
