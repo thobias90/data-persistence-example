3897913558a0237937346977ff0aa5ba681f1a8c
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2023-08-26 14:32:03 -0300

    feat: add main activity callbacks

--- app/src/main/java/com/stahlt/data_persistence/MainActivity.kt
+++ app/src/main/java/com/stahlt/data_persistence/MainActivity.kt
@@ -2,10 +2,17 @@ package com.stahlt.data_persistence
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun btAddOnClick(view: View) {}
+    fun btChangeOnClick(view: View) {}
+    fun btDeleteOnClick(view: View) {}
+    fun btSearchOnClick(view: View) {}
+    fun btListOnClick(view: View) {}
 }
