8d185bed5170ec5cc8dd113a3d7bbcc753785a1f
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2023-09-08 16:56:05 -0300

    refactor: update to use database handler

--- app/src/main/java/com/stahlt/data_persistence/MainActivity.kt
+++ app/src/main/java/com/stahlt/data_persistence/MainActivity.kt
@@ -1,20 +1,17 @@
 package com.stahlt.data_persistence
 
-import android.content.ContentValues
-import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.EditText
 import android.widget.Toast
-import java.lang.StringBuilder
+import com.stahlt.data_persistence.database.DatabaseHandler
+import com.stahlt.data_persistence.entity.Student
 
 class MainActivity : AppCompatActivity() {
     private lateinit var etCode: EditText
     private lateinit var etName: EditText
     private lateinit var etPhone: EditText
-
-    private lateinit var dataBase: SQLiteDatabase
+    private lateinit var database: DatabaseHandler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,62 +20,38 @@ class MainActivity : AppCompatActivity() {
         etName = findViewById(R.id.etName)
         etPhone = findViewById(R.id.etPhone)
 
-        dataBase = SQLiteDatabase.openOrCreateDatabase(this.getDatabasePath("dbfile.sqlite"),
-            null)
-
-        dataBase.execSQL("CREATE TABLE IF NOT EXISTS student (_id INTEGER " +
-                "PRIMARY KEY AUTOINCREMENT, name TEXT, phone TEXT)")
-
+        database = DatabaseHandler(this)
     }
 
-    fun btAddOnClick(view: View) {
-        val register = ContentValues()
-        register.put("name", etName.text.toString())
-        register.put("phone", etPhone.text.toString())
-        dataBase.insert("student", null, register)
+    fun btAddOnClick() {
+        val student = Student(0, etName.text.toString(), etPhone.text.toString())
+        database.create(student)
         Toast.makeText(this, "Element was added successfully",
             Toast.LENGTH_SHORT).show()
     }
-    fun btChangeOnClick(view: View) {
-        val code = etCode.text.toString()
-
-        val register = ContentValues()
-        register.put("name", etName.text.toString())
-        register.put("phone", etPhone.text.toString())
-        dataBase.update("student", register, "_id=${code}", null)
+    fun btChangeOnClick() {
+        val student = Student(etCode.text.toString().toInt(), etName.text.toString(),
+            etPhone.text.toString())
+        database.update(student)
         Toast.makeText(this, "Element Changed", Toast.LENGTH_SHORT).show()
     }
-    fun btDeleteOnClick(view: View) {
-        val code = etCode.text.toString()
-
-        dataBase.delete("student", "_id=${code}", null)
+    fun btDeleteOnClick() {
+        val code = etCode.text.toString().toInt()
+        database.delete(code)
         Toast.makeText(this, "Element Deleted", Toast.LENGTH_SHORT).show()
     }
-    fun btSearchOnClick(view: View) {
-        val code = etCode.text.toString()
-
-        val registers = dataBase.query("student", null, "_id=${etCode.text}",
-            null, null, null, null)
-        if (registers.moveToNext()) {
-            etName.setText( registers.getString(1))
-            etPhone.setText(registers.getString(2))
+    fun btSearchOnClick() {
+        val code = etCode.text.toString().toInt()
+        val student: Student? = database.read(code)
+        if (student != null) {
+            etName.setText(student.name)
+            etPhone.setText(student.phone)
         } else {
             Toast.makeText(this, "Register not found", Toast.LENGTH_SHORT).show()
         }
-
     }
-    fun btListOnClick(view: View) {
-        val registers = dataBase.query("student", null, null,
-            null, null, null, null)
-        var output = StringBuilder()
-        while(registers.moveToNext()) {
-            output.append(registers.getInt(0))
-            output.append(" ")
-            output.append(registers.getString(1))
-            output.append(" ")
-            output.append(registers.getString(2))
-            output.append("\n")
-        }
-        Toast.makeText(this, output.toString(), Toast.LENGTH_LONG).show()
+    fun btListOnClick() {
+        val databaseList: String = database.list()
+        Toast.makeText(this, databaseList, Toast.LENGTH_LONG).show()
     }
 }
