50208c3d0b02867e8f1f4c008e01f6db6f66f5c4
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2023-09-16 11:02:58 -0300

    refactor: update button logic

--- app/src/main/java/com/stahlt/data_persistence/MainActivity.kt
+++ app/src/main/java/com/stahlt/data_persistence/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.stahlt.data_persistence
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.stahlt.data_persistence.database.DatabaseHandler
 import com.stahlt.data_persistence.entity.Student
 
@@ -13,6 +14,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var etCode: EditText
     private lateinit var etName: EditText
     private lateinit var etPhone: EditText
+    private lateinit var btDelete: Button
+    private lateinit var btSearch: Button
     private lateinit var database: DatabaseHandler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,41 +24,64 @@ class MainActivity : AppCompatActivity() {
         etCode = findViewById(R.id.etCode)
         etName = findViewById(R.id.etName)
         etPhone = findViewById(R.id.etPhone)
+        btSearch = findViewById(R.id.btSearch)
+        btDelete = findViewById(R.id.btDelete)
+
+        if (intent.getIntExtra("code", 0) != 0) {
+            etCode.setText(intent.getIntExtra("code", 0).toString())
+            etName.setText(intent.getStringExtra("name").toString())
+            etPhone.setText(intent.getStringExtra("phone").toString())
+        } else {
+            btSearch.visibility = View.INVISIBLE
+            btDelete.visibility = View.INVISIBLE
+        }
 
         database = DatabaseHandler(this)
     }
 
-    fun btAddOnClick(view: View) {
-        val student = Student(0, etName.text.toString(), etPhone.text.toString())
-        database.create(student)
-        Toast.makeText(this, "Element was added successfully",
-            Toast.LENGTH_SHORT).show()
-    }
-    fun btChangeOnClick(view: View) {
-        val student = Student(etCode.text.toString().toInt(), etName.text.toString(),
-            etPhone.text.toString())
-        database.update(student)
-        Toast.makeText(this, "Element Changed", Toast.LENGTH_SHORT).show()
+    fun btSaveOnClick(view: View) {
+        if (etCode.text.isEmpty()) { // Add element
+            val student = Student(
+                0,
+                etName.text.toString(),
+                etPhone.text.toString())
+            database.create(student)
+            Toast.makeText(this, "Element was added successfully",
+                Toast.LENGTH_SHORT).show()
+        } else { // Change element
+            val student = Student(
+                etCode.text.toString().toInt(),
+                etName.text.toString(),
+                etPhone.text.toString())
+            database.update(student)
+            Toast.makeText(this, "Element Changed", Toast.LENGTH_SHORT).show()
+        }
+        finish()
     }
     fun btDeleteOnClick(view: View) {
         val code = etCode.text.toString().toInt()
         database.delete(code)
         Toast.makeText(this, "Element Deleted", Toast.LENGTH_SHORT).show()
+        finish()
     }
     fun btSearchOnClick(view: View) {
-        val code = etCode.text.toString().toInt()
-        val student: Student? = database.read(code)
-        if (student != null) {
-            etName.setText(student.name)
-            etPhone.setText(student.phone)
-        } else {
-            Toast.makeText(this, "Register not found", Toast.LENGTH_SHORT).show()
+        val etCodeSearch = EditText(this)
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Code")
+        builder.setView(etCodeSearch)
+        builder.setCancelable(false)
+        builder.setNegativeButton("Close", null)
+        builder.setPositiveButton("Search") { _, _ ->
+            val code = etCodeSearch.text.toString().toInt()
+            val student: Student? = database.read(code)
+            if (student != null) {
+                etCode.setText(code.toString())
+                etName.setText(student.name)
+                etPhone.setText(student.phone)
+            } else {
+                Toast.makeText(this, "Register not found", Toast.LENGTH_SHORT).show()
+            }
         }
-    }
-    fun btListOnClick(view: View) {
-//        val databaseList: String = database.list()
-//        Toast.makeText(this, databaseList, Toast.LENGTH_LONG).show()
-        val intent = Intent(this, ListActivity::class.java)
-        startActivity(intent)
+        builder.show()
     }
 }
